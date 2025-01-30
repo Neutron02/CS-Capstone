bd681f5610a99ce08ed2bee288efbd6a8e9e5c90
Wolf1716 <Wolf181716@outlook.com>
2025-01-30 10:21:51 -0500

    Formatted Cards
    ~Added Cards & Formatted.

--- cscapstone/src/Components/MarketplaceGrid.js
+++ cscapstone/src/Components/MarketplaceGrid.js
@@ -3,10 +3,16 @@ import MarketplaceCard from './MarketplaceCard';
 
 /* Task is dynamic rendering based on source of data for marketplace cards */
 // Task is modifying the size and placement of grids based on screen size, or based on card content
-
 function MarketplaceGrid() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-start">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-start w-full ml-6 mr-6">
+      <MarketplaceCard />
+      <MarketplaceCard />
+      <MarketplaceCard />
+      <MarketplaceCard />
+      <MarketplaceCard />
+      <MarketplaceCard />
+      <MarketplaceCard />
       <MarketplaceCard />
       <MarketplaceCard />
       <MarketplaceCard />
